a293683aee9ccd7b52e7cb41c892d6e67218daf0
Anjie Yang <digmouse233@gmail.com>
2024-04-20 13:55:06 -0400

    Update download logic

--- video_downloader.py
+++ video_downloader.py
@@ -7,7 +7,8 @@ class VideoDownloader:
     @staticmethod
     def download_video(save_dir, video_id, resolution, server_id):
         save_path = os.path.join(save_dir, f"vid_{video_id}.mp4")
-        format_code = "bestvideo[height<=480][ext=mp4]+bestaudio[ext=m4a]/best[height<=480]" if resolution == 'sd' else "bestvideo[height>=720][ext=mp4]+bestaudio[ext=m4a]/best[height>=720]"
+        # format_code = "bestvideo[height<=480][ext=mp4]+bestaudio[ext=m4a]/best[height<=480]" if resolution == 'sd' else "bestvideo[height>=720][ext=mp4]+bestaudio[ext=m4a]/best[height>=720]"
+        format_code = "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
         command = [
             "yt-dlp",
             f"https://www.youtube.com/watch?v={video_id}",
