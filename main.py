38dbec590edd51c4bcaedbff48597cafe6391ff2
Anjie Yang <digmouse233@gmail.com>
2024-04-22 06:13:17 -0400

    Fix bugs

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ def setup_rabbitmq_connection():
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(
             host=os.getenv('RABBITMQ_HOST'),
-            port=os.getenv('RABBITMQ_PORT')
+            port=os.getenv('RABBITMQ_PORT'),
+            heartbeat=600,
+            blocked_connection_timeout=300,
+            connection_attempts=5,
+            retry_delay=5
         ))
     channel = connection.channel()
     return connection, channel
@@ -29,29 +33,25 @@ def request_task(channel, server_id):
 def handle_download(channel, method, properties, body, args):
     print(f"Received raw message: {body.decode()}")
     data = json.loads(body.decode())
-    print(data)
-
     video_id = data['video_id']
     resolution = data['resolution']
+    save_dir, server_id = args
 
-    save_dir = args[0]
-    server_id = args[1]
-
-    print(
-        f"Received task for video_id {video_id} on server {server_id}.")
-
-    response = VideoDownloader.download_video(
-        save_dir, video_id, resolution, server_id)
-
-    channel.basic_publish(
-        exchange='',
-        routing_key='video_download_response_queue',
-        body=json.dumps(response),
-        properties=pika.BasicProperties(delivery_mode=2)
-    )
-    print(
-        f"Task for video_id {video_id} processed with status: {response['status_code']}.")
-    channel.basic_ack(delivery_tag=method.delivery_tag)
+    try:
+        response = VideoDownloader.download_video(
+            save_dir, video_id, resolution, server_id)
+        print(
+            f"Task for video_id {video_id} processed with status: {response['status_code']}.")
+        channel.basic_publish(
+            exchange='',
+            routing_key='video_download_response_queue',
+            body=json.dumps(response),
+            properties=pika.BasicProperties(delivery_mode=2)
+        )
+    except Exception as e:
+        print(f"Error processing download for video_id {video_id}: {str(e)}")
+    finally:
+        channel.basic_ack(delivery_tag=method.delivery_tag)
 
 
 def main():
--- video_downloader.py
+++ video_downloader.py
@@ -7,35 +7,43 @@ class VideoDownloader:
     @staticmethod
     def download_video(save_dir, video_id, resolution, server_id):
         save_path = os.path.join(save_dir, f"vid_{video_id}.mp4")
-        # format_code = "bestvideo[height<=480][ext=mp4]+bestaudio[ext=m4a]/best[height<=480]" if resolution == 'sd' else "bestvideo[height>=720][ext=mp4]+bestaudio[ext=m4a]/best[height>=720]"
         format_code = "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
         command = [
             "yt-dlp",
             f"https://www.youtube.com/watch?v={video_id}",
-            "--output", f"{save_path}",
+            "--output", save_path,
             "-f", format_code,
             "--external-downloader", "aria2c",
-            "--external-downloader-args", "aria2c:-x 4 -s 4 -k 5M --console-log-level=warn --quiet=true",
+            "--external-downloader-args", "aria2c:-x 4 -s 4 -k 1M --console-log-level=warn --quiet",
             "--quiet",
-            "--concurrent-fragments", "8",
-            "--buffer-size", "32K",
-            "--http-chunk-size", "1M",
-            "--fragment-retries", "3",
+            "--user-agent", user_agent,
+            "--concurrent-fragments", "4",
+            "--buffer-size", "16K",
+            "--http-chunk-size", "512K",
+            "--fragment-retries", "2",
+            "--check-formats"
         ]
         process = subprocess.Popen(
             command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True
         )
         stdout, stderr = process.communicate()
 
-        if process.returncode == 0:
+        if os.path.exists(save_path) and os.path.getsize(save_path) > 0:
             logging.info("Video %s downloaded successfully to %s",
                          video_id, save_path)
+            return {
+                'status_code': 0,
+                'video_id': video_id,
+                'location': save_path,
+                'server_id': server_id
+            }
         else:
-            logging.error("Error downloading video %s: %s", video_id, stdout)
-
-        return {
-            'status_code': process.returncode,
-            'video_id': video_id,
-            'location': save_path,
-            'server_id': server_id
-        }
+            logging.error(
+                "Failed to download video %s. Error: %s", video_id, stdout)
+            return {
+                'status_code': 1,
+                'video_id': video_id,
+                'location': save_path,
+                'server_id': server_id
+            }
